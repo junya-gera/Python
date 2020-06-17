89ace673e351baa85ee58d03c94d39647ce26859
junya-gera <aa106921@gmail.com>
2020-06-17 21:26:00 +0900

    #11 return pass

--- myapp2.py
+++ myapp2.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # 関数
 def say_hi(name,age = 20):  # age=20は初期値
   print("hi {0} ({1})".format(name,age))
@@ -5,3 +6,12 @@ def say_hi(name,age = 20):  # age=20は初期値
 say_hi("tom",23)
 say_hi("bob",21)
 say_hi("steve")
+
+def say_hello():
+  # 関数の中身がない場合、もしくは後で書くので何か書いておきたい場合はpassとかく
+  # Noneが返る
+  pass
+  # return "hello"
+
+msg = say_hello()
+print(msg)
