814eb90e1ca9beb2b6f3b5028f0dcf045dc650d3
junya-gera <aa106921@gmail.com>
2020-06-20 07:39:21 +0900

    #14 コンストラクタ

--- class.py
+++ class.py
@@ -1,14 +1,21 @@
 class User:
-  pass
+  # コンストラクタ
+  def __init__(self,name):
+    # インスタンス変数
+    self.name = name
 
+# tom = User()
+# tom.name = "tom"
+# tom.score = 20
 
-tom = User()
-tom.name = "tom"
-tom.score = 20
+# bob = User()
+# bob.name = "bob"
+# bob.level = 5
 
-bob = User()
-bob.name = "bob"
-bob.level = 5
+# print(tom.name)
+# print(bob.level)
+tom = User("tom")
+bob = User("bob")
 
 print(tom.name)
-print(bob.level)
+print(bob.name)
