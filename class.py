265f7c82edd3f787ce9f4eea9add5bd61dbcc7ab
junya-gera <aa106921@gmail.com>
2020-06-21 23:34:31 +0900

    #15 クラス変数

--- class.py
+++ class.py
@@ -1,6 +1,11 @@
+# -*- coding: utf-8 -*-
+
 class User:
+  # クラス変数
+  count = 0
   # コンストラクタ
   def __init__(self,name):
+    User.count += 1
     # インスタンス変数
     self.name = name
 
@@ -14,8 +19,12 @@ class User:
 
 # print(tom.name)
 # print(bob.level)
+print(User.count)
 tom = User("tom")
 bob = User("bob")
+print(User.count)
 
 print(tom.name)
+print(tom.count)
 print(bob.name)
+print(bob.count)
