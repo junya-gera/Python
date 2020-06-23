c5ee7eed9f7595aa002ec9b081affff6a593b659
junya-gera <aa106921@gmail.com>
2020-06-23 20:23:41 +0900

    #16 クラスメソッド

--- class.py
+++ class.py
@@ -11,7 +11,10 @@ class User:
 
   def say_hi(self):
     print("hi {0}".format(self.name))
-
+ # クラスメソッド
+  @classmethod
+  def show_info(cls):
+      print("{0} instances".format(cls.count))
 # tom = User()
 # tom.name = "tom"
 # tom.score = 20
